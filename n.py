e3a70d7aeda2d2777bae16a6fcf3b3b6e03596ce
Diegofergamboa <>
2021-06-06 19:58:39 +0000

    Final_version . A versión that hives to the user a better experiencies using whiles

--- n.py
+++ n.py
@@ -1,4 +1,5 @@
-"from art import logo"
+from art import logo
+print(logo)
 #Add 
 def add(n1, n2) :
     return n1 + n2
@@ -23,13 +24,26 @@ operations = {
     "*" : mul ,
     "/" : divide 
 }
-num1 = int(input('Type the first number to operate: '))
-num2 = int(input('Type the second number to operate: '))
-for i in operations :
-    print(i)
-    
-operation_symbol = input('Choose any of the symbols showed above:\n')
-#print(operation_symbol)
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
-print(f'The result of the operation is, {answer} ') 
+#
+#while should_continue :
+def Calculator():
+    num1 = float(input('Type the first number to operate: '))
+    for i in operations :
+        print(i)
+    should_continue = True 
+    while should_continue :       
+        operation_symbol = input('Choose a symbol from the symbols showed above:\n')
+        num2 = float(input('Type the next number to operate: '))
+        #print(operation_symbol)
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f'{num1} {operation_symbol} {num2} = {answer}.') 
+        #Trying to reach out the program
+        #Presione Si para hacer otro cálculo con el numero anteriormente calculado o n para hacer otro cálculo
+        if input('Do you want to made another operation with {answer} or do you want to made another new operation? Type "y" by yes or "n" to have another calculation' ).lower() == 'y' :
+            num1 = answer
+        
+        else :
+            should_continue = False        
+            Calculator()
+Calculator()
